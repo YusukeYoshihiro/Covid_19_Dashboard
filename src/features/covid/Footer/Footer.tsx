4fd3fb84ede7f09e12549dfa1312534c3d34d981
YusukeYoshihiro <yusuke.10.25.61@gmail,com>
2021-03-03 01:54:49 -0800

    modified footer

--- src/features/covid/Footer/Footer.tsx
+++ src/features/covid/Footer/Footer.tsx
@@ -5,7 +5,7 @@ import styles from './Footer.module.css';
 const Footer:React.FC = () => {
   return (
     <footer className={styles.footerStyles}>
-       Yusuke Yoshihiro @ 2021 All Right Reserved <br/>Referring <a href="https://covid19.mathdro.id/api"> &nbsp; API - Covid-19 Mathdro.id </a>
+      Copyright &copy; 2021 Yusuke Yoshihiro <br/>Referring <a href="https://covid19.mathdro.id/api"> &nbsp; API - Covid-19 Mathdro.id </a>
     </footer>
   )
 }
